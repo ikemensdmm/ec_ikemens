7144a31ff6f6372c332283a1e8baaccca615c902
EC2 Default User <ec2-user@ip-172-31-15-70.ec2.internal>
2021-12-17 05:53:07 +0000

    Add Address

--- nagano_cake/config/routes.rb
+++ nagano_cake/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  
+  # customer
+  namespace :customer do
+  	resources :addresses,only: [:index,:create,:edit,:update,:destroy]
+  	
+    end
+    
   devise_for :admins
   devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  
+  end
