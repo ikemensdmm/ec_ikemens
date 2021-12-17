c25d170292202c4958906decaefe63dd9b8ed49f
NMGHDWC <mnwmnwmnw2011@gmail.com>
2021-12-17 05:20:12 +0000

    [Add] genres

--- nagano_cake/config/routes.rb
+++ nagano_cake/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :genres
+  end
+  namespace :customer do
+    resources :orders
+    get 'orders/confirmation'
+    get 'orders/message'
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
