691c65668ad0311dfe48aa55fad0510de5c455c9
EC2 Default User <ec2-user@ip-172-31-15-70.ec2.internal>
2021-12-18 06:00:25 +0000

    Add admin items new create

---
+++ nagano_cake/app/controllers/admin/items_controller.rb
@@ -0,0 +1,26 @@
+class Admin::ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def create
+    @item = Item.new(items_params)
+    if @item.save
+      flash[:notice] = "新商品を登録しました"
+      redirect_to admin_items_path(@item)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:name, :image, :description, :price, :is_status, :genre_id)
+  end
+end
--- nagano_cake/config/routes.rb
+++ nagano_cake/config/routes.rb
@@ -2,19 +2,17 @@ Rails.application.routes.draw do
 
   
   # customer
-  namespace :customer do
-  	resources :addresses,only: [:index,:create,:edit,:update,:destroy]
-  	
-    end
-    
-
   namespace :admin do
     resources :genres
+    resources :items
+    post 'items/new'
   end
   namespace :customer do
     resources :orders
     get 'orders/confirmation'
     get 'orders/message'
+    resources :addresses,only: [:index,:create,:edit,:update,:destroy]
+    
   end
 
   devise_for :admins
