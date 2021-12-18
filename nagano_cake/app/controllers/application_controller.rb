28c959a8c15359dfe0a0b6e25bc5dd898f481b29
EC2 Default User <ec2-user@ip-172-31-38-68.ec2.internal>
2021-12-18 06:34:49 +0000

    [first commit

--- nagano_cake/app/controllers/application_controller.rb
+++ nagano_cake/app/controllers/application_controller.rb
@@ -1,2 +1,43 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_in_path_for(resource)
+      if current_customer
+        flash[:notice] = "ログインに成功しました"
+        customer_homes_top_path  # 好きなパスにしてください（まだ画像が投稿されてないのでtopページが表示できないです）
+      else
+        flash[:notice] = "新規登録完了しました。次に名前を入力してください"
+        customer_homes_top_path # 好きなパスにしてください（まだ画像が投稿されてないのでtopページが表示できないです）
+      end
+    end
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:kana_first_name])
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:kana_last_name])
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:postcode])
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
+    end
 end
--- nagano_cake/config/routes.rb
+++ nagano_cake/config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  namespace :customer do
+    get 'homes/top'
+    get 'homes/about'
+  end
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  devise_for :customers,　skip: [:passwords,], controllers: {
+  registrations: "customer/registrations",
+  sessions: 'customer/sessions'
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
