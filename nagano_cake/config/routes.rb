2e052d74b3e89fde7954947d03035f22dfb7edb6
NMGHDWC <mnwmnwmnw2011@gmail.com>
2021-12-18 06:13:11 +0000

    [Add] customer/items/index show

--- nagano_cake/config/routes.rb
+++ nagano_cake/config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-
-  
-  # customer
-  namespace :customer do
-  	resources :addresses,only: [:index,:create,:edit,:update,:destroy]
-  	
-    end
-    
-
   namespace :admin do
     resources :genres
   end
@@ -15,9 +6,10 @@ Rails.application.routes.draw do
     resources :orders
     get 'orders/confirmation'
     get 'orders/message'
+    resources :items
+    resources :addresses,only: [:index,:create,:edit,:update,:destroy]
   end
-
   devise_for :admins
   devise_for :customers
-  
-  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
