d25b087633fcd016146078c63515cca0daf1636d
NMGHDWC <mnwmnwmnw2011@gmail.com>
2021-12-20 02:27:55 +0000

    [Fix] customer/items/index show [Add] customer/orders/index show

--- nagano_cake/config/initializers/application_controller_renderer.rb
+++ nagano_cake/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e8f0815c85ec15909f58c54b6ecb7b4953e2eab289d2c6a4d1e3cddcccb3d47e0896c8305cd359accb34187ddc70624351e86641180bbac83847b40941d1b4f0'
