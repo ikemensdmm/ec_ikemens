212e30ca3534bc323e7603df104cd4f99b7eb891
moyama55 <moyama04@gmail.com>
2021-12-22 08:14:09 +0000

    aaa

--- nagano_cake/app/controllers/admin/items_controller.rb
+++ nagano_cake/app/controllers/admin/items_controller.rb
@@ -6,10 +6,12 @@ class Admin::ItemsController < ApplicationController
 
   def index
     @items = Item.all
+    @item_count = Item.count
   end
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
   def edit
