38ee8979035b311b8abff3ea0dd2bfd928471635
EC2 Default User <ec2-user@ip-172-31-38-68.ec2.internal>
2021-12-22 05:57:47 +0000

    test commit

--- nagano_cake/app/controllers/customer/cart_items_controller.rb
+++ nagano_cake/app/controllers/customer/cart_items_controller.rb
@@ -11,10 +11,17 @@ class Customer::CartItemsController < ApplicationController
 end
 
 def create
-  @cart_item = current_customer.cart_items.new(cart_item_params)
-  @cart_item.save
-  redirect_to customer_cart_items_path
-end
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+       cart_item.quantity += params[:cart_item][:quantity].to_i
+       cart_item.save
+       redirect_to customer_cart_items_path
+    else
+       @cart_item.save
+       redirect_to customer_cart_items_path
+    end
+  end
 
 	def destroy
     @cart_item = CartItem.find(params[:id])
--- nagano_cake/app/controllers/customer/sessions_controller.rb
+++ nagano_cake/app/controllers/customer/sessions_controller.rb
@@ -27,9 +27,9 @@ class Customer::SessionsController < Devise::SessionsController
     protected
 
   def reject_customer
-    @customer = Customer.find_by(name: params[:customer][:email])
+    @customer = Customer.find_by(params[:password][:email])
     if @customer
-      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_withdrawal == false)
+      if @customer.valid_password?(params[:email][:password]) && (@customer.is_withdrawal == false)
         flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
         redirect_to new_customer_registration_path
       else
